456d8ed7ad22eff84d10b1dce8493090bcec2cad
Alexander Edward Merejildo Tomala <amerejil@espol.edu.ec>
2019-07-26 23:57:47 -0500

    Subida de codigo a github

---
+++ Contro_LEDs_RGB/Contro_LEDs_RGB/Contro_LEDs_RGB.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Contro_LEDs_RGB.ino
+ Created:	26/07/2019 23:54:28
+ Author:	amere
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
